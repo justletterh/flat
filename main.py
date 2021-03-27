5ae60e618e5df13bcefe0dab276d40845c56c5f0
justletterh <63440442+justletterh@users.noreply.github.com>
2021-03-27 14:15:21 -0500

    h

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import os
 from string import ascii_lowercase as abc
+from shutil import copy2 as cp
+from shutil import rmtree as rmdir
 
 fp=lambda *x:os.path.join(".",*x)
 j=lambda *x:os.path.join(*x)
+fn=lambda f:f.split(os.path.sep)[-1]
+ext=lambda f:f'.{fn(f).split(".")[-1]}'
 
 outdir=fp("games")
 indir=fp("a")
+exts=[f".{x}" for x in "gba,gb,gbc,nds,3ds".split(",")]
+
+def look(f):
+    do=lambda f:[[j(x[0],xx) for xx in x[2]] for x in os.walk(f)]
+    l=[]
+    for i in do(f):
+        for ii in i:
+            if ext(ii) in exts:
+                l.append(ii)
+    return l
 
 def main():
-    print("Hello, World!!!")
+    l=look(indir)
+    for i in l:
+        cp(i,fp(outdir,fn(i)))
 
 def init():
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
+    if os.path.exists(outdir):
+        rmdir(outdir)
+    os.mkdir(outdir)
     
     #remove later
     if not os.path.exists(fp(*list(abc))):
